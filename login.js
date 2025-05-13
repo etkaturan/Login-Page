aaa39918d6a51c45e08e1344757d4e94d82f0c0d
etkaturan <metkaturanua@gmail.com>
2025-05-13 19:39:43 +0200

    Update

--- login.js
+++ login.js
@@ -1,4 +1,28 @@
 function login() {
-    alert("Merhaba")
+    
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let dogruKullaniciAdi = "Etka";
+    let dogruSifre = "password";
+
+    if (userName === dogruKullaniciAdi && password === dogruSifre) {
+        window.location.href = "main.html"
+    }
+    else if (userName === "") {
+        alert("Enter Username Please!")
+    }
+    else if (password === "") {
+        alert("Enter Password Please!")
+    }
+    else if (userName === "Admin") {
+        alert("Welcome Admin!")
+    }
+    else {
+        alert("Try Again!")
+    }
+
 }
+
+
 
